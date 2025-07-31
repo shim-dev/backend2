adc34bdd92a57be903215cd9eb32f4d7ed877579
JHJ-lab <jhj072787@gmail.com>
2025-07-31 11:53:09 +0900

    1. 이메일 및 아이디 중복 체크 추가

--- routes/signup/user_routes.py
+++ routes/signup/user_routes.py
@@ -32,7 +32,38 @@ def register_user():
     except Exception as e:
         print("[ERROR]", str(e))
         return jsonify({"error": "Database insert failed"}), 500
-    
+
+# 이메일 중복 확인
+@user_bp.route('/check-email', methods=['POST'])
+def check_email():
+    data = request.get_json()
+    email = data.get("email")
+
+    if not email:
+        return jsonify({"error": "Email is required"}), 400
+
+    user = mongo.db.users.find_one({"email": email})
+    if user:
+        return jsonify({"exists": True}), 200
+    else:
+        return jsonify({"exists": False}), 200
+   
+# 닉네임 중복 확인
+@user_bp.route('/check-nickname', methods=['POST'])
+def check_nickname():
+    data = request.get_json()
+    nickname = data.get("nickname")
+
+    if not nickname:
+        return jsonify({"error": "Nickname is required"}), 400
+
+    user = mongo.db.users.find_one({"nickname": nickname})
+    if user:
+        return jsonify({"exists": True}), 200
+    else:
+        return jsonify({"exists": False}), 200
+
+   
 @user_bp.route('/login', methods=['POST'])
 def login_user():
     data = request.get_json()
